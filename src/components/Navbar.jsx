6118195b07b1d05ca5e6350303c6336587541b7b
christhecoolguy <christopherdiggs1@gmail.com>
2023-12-12 13:07:28 -0600

    christmas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
+import { NavLink } from "react-router-dom"
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="header">
+      <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"> 
+      <p className="christmas-gradient-text">WWM</p>
+
+
+      </NavLink>
+
+    </header>
   )
 }
 

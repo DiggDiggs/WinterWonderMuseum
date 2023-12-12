0a06326463de97880abf5e65040dd9d5b339e5d8
christhecoolguy <christopherdiggs1@gmail.com>
2023-12-12 13:45:06 -0600

    names

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom"
+const Navbar = () => {
+  return (
+    <header className="header">
+  <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+    <p className="christmas-gradient-text">WWM</p>
+  </NavLink>
+
+  <nav className="flex text-lg gap-7 font-medium">
+    <NavLink to="chris" className={({ isActive }) => (isActive ? 'text-green-700' : 'text-black')}>
+      Chris
+    </NavLink>
+
+    <NavLink to="dom" className={({ isActive }) => (isActive ? 'text-green-700' : 'text-black')}>
+      Dom
+    </NavLink>
+
+    <NavLink to="alex" className={({ isActive }) => (isActive ? 'text-green-700' : 'text-black')}>
+      Alex
+    </NavLink>
+  </nav>
+</header>
+
+  )
+}
+
+export default Navbar

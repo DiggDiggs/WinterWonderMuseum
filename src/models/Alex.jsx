267c9a5f9ede7e4594beb02fad23aa4ccf7815ee
Tribeoftech <gratefulgrowing333@gmail.com>
2023-12-19 16:43:24 -0600

    entered new file from animation creator dshoot1_default. into ALex profile.

--- src/models/Alex.jsx
+++ src/models/Alex.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
-
-import { useGLTF } from '@react-three/drei';
-import alexScene from '../assets/3d/SnowAstroAlex.glb';
+import { useAnimations, useGLTF } from '@react-three/drei';
+import React, { useEffect, useRef } from 'react';
+import alexScene from '../assets/3d/dshoot1_default.glb';
 
 const Alex = () => {
     const { scene, animations } = useGLTF(alexScene);
-  return (
-    <mesh position={[-1, -2, -3]} scale={[.5, .5,.5]}>
+    const ref = useRef();
+    const { actions } = useAnimations(animations, ref);
+    useEffect(() => {
+      const onAnimationLoad = () => {
+        console.log('Model and animations are loaded:', animations);
+      };
+  
+      ref.current.addEventListener('model-loaded', onAnimationLoad);
+  
+      return () => {
+        ref.current.removeEventListener('model-loaded', onAnimationLoad);
+      };
+    }, [ref, animations]);
+  
+    useEffect(() => {
+      if (actions && actions["dshoot1_default.glb"]) {
+        if (isRotating) {
+          actions["dshoot1_default.glb"].play();
+        } else {
+          actions["dshoot1_default.glb"].stop();
+        }
+      }
+    }, [actions, isRotating]);
+  
+    return (
+      <mesh {...props} position={[-1, -2, -3]} scale={[.5, .5,.5]}>
         <primitive object={scene} />
 
     </mesh>

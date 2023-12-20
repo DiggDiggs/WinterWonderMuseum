953aa22994c23362acb28f9b73bc46b23753a6c8
Dom <dominickckeeling@gmail.com>
2023-12-20 12:55:43 -0600

    dom is now dancing

--- src/models/dom3d.jsx
+++ src/models/dom3d.jsx
@@ -2,7 +2,6 @@ import React, { useRef, useEffect } from 'react'
 import { useAnimations, useGLTF } from '@react-three/drei';
 import domScene from '../assets/3d/3d-dom.glb';
 
-
 const Dom = ({isRotating, ...props }) => {
     const { scene, animations } = useGLTF(domScene);
     const ref = useRef();
@@ -13,27 +12,36 @@ const Dom = ({isRotating, ...props }) => {
         console.log('Model and animations are loaded:', animations);
       };
 
-      ref.current.addEventListener('model-loaded', onAnimationLoad);
+      const currentRef = ref.current;
+
+      if (currentRef) {
+        currentRef.addEventListener('model-loaded', onAnimationLoad);
+      }
 
       return () => {
-        ref.current.removeEventListener('model-loaded', onAnimationLoad);
+        if (currentRef) {
+          currentRef.removeEventListener('model-loaded', onAnimationLoad);
+        }
       };
     }, [ref, animations]);
 
     useEffect(() => {
-      if (actions && actions["3d-dom"]) {
+      if (actions && actions["IMG_2521"] && actions["IMG_2521"].play && actions["IMG_2521"].stop) {
         if (isRotating) {
-          actions["3d-dom"].stop();
+          actions["IMG_2521"].play();
+        } else {
+          actions["IMG_2521"].stop();
         }
       }
     }, [actions, isRotating]);
 
   return (
-    <mesh {...props} ref={ref} position={[5, -4.5, -73]} scale={[2, 2, 2]}>
-        <primitive object={scene} />
-
-    </mesh>
+    <group>
+      <mesh {...props} ref={ref} position={[5, -4.5, -73]} scale={[2, 2, 2]}>
+          <primitive object={scene} />
+      </mesh>
+    </group>
   );
 };
 
-export default Dom
+export default Dom;

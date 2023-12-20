10844bd7cb3800c174dec5b56431da489c91b7cc
Dom <dominickckeeling@gmail.com>
2023-12-19 23:07:09 -0600

    animating 3ddom to dance. still need to add to Home.jsx still

--- src/models/dom3d.jsx
+++ src/models/dom3d.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-
+import React, { useRef, useEffect } from 'react'
+import { useAnimations, useGLTF } from '@react-three/drei';
 import domScene from '../assets/3d/3d-dom.glb';
-import { useGLTF } from '@react-three/drei';
 
-const Dom = () => {
+
+const Dom = ({osRotating, ...props }) => {
     const { scene, animations } = useGLTF(domScene);
+    const ref = useRef();
+    const { actions } = useAnimations(animations, ref);
+
+    useEffect(() => {
+      const onAnimationLoad = () => {
+        console.log('Model and animations are loaded:', animations);
+      };
+
+      ref.current.addEcentListener('model-loaded', onAnimationsLoad);
+
+      return () => {
+        ref.current.removeEventListener('model-loaded', onAnimationLoad);
+      };
+    }, [ref, anumations]);
+    useEffect(() => {
+      if (actions && actions["3d-dom"]) {
+        if (isRotating) {
+          actions["3d-dom"].stop();
+        }
+      }
+    }, [actions, isRotating]);
+
   return (
-    <mesh position={[-.15, -1.1, -5.]} scale={[.18, .18, .03]}>
+    <mesh {...props} ref={ref} position={[-.15, -1.1, -5.]} scale={[.18, .18, .03]}>
         <primitive object={scene} />
 
     </mesh>
-  )
-}
+  );
+};
 
 export default Dom

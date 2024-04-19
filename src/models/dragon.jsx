bdb73638888c5bcb9f6f28529c4948ed0280ba0b
Christopher Diggs <65854363+DiggDiggs@users.noreply.github.com>
2024-04-18 21:25:52 -0500

    Update dragon.jsx

--- src/models/dragon.jsx
+++ src/models/dragon.jsx
@@ -1,16 +1,1 @@
-import React from 'react'
 
-import dragonScene from '../assets/3d/dragon.glb';
-import { useAnimations, useGLTF } from '@react-three/drei';
-
-const Dragon = () => {
-    const { scene, animations } = useGLTF(dragonScene);
-
-  return (
-    <mesh position={[-1, 2, -3]} scale={[1, 1, 1]}>
-      <primitive object={scene} />
-    </mesh>
-  )
-}
-
-export default Dragon

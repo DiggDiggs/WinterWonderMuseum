bb65efe81a28907f29c0f52cc9bb80a1a402a215
Christopher Diggs <65854363+DiggDiggs@users.noreply.github.com>
2024-04-18 21:26:32 -0500

    Update code

--- src/models/Mail.jsx
+++ src/models/Mail.jsx
@@ -1,16 +1,1 @@
-import React from 'react';
 
-import { useGLTF } from '@react-three/drei';
-import mailScene from '../assets/3d/small_winter_scene.glb';
-
-const Mail = () => {
-    const { scene, animations } = useGLTF(mailScene);
-  return (
-    <mesh position={[-2, -5, -77]} scale={[.150, .150, .150]}>
-        <primitive object={scene} />
-
-    </mesh>
-  )
-}
-
-export default Mail;
